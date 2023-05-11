9935eeb2da0bb395627fe54d2c7dac9f4fe76c35
Franklin Hau <fhauchi@gmail.com>
2023-05-11 15:29:59 -0600

    fetched data is an array itself not an object

--- main.js
+++ main.js
@@ -1,26 +1,30 @@
-console.log('main js works')
-
 fetch('http://localhost:3000/affirmations')
-    .then(response => response.json())
-   // .then(json => console.log(json))
-    .then(affirmations) => {
-        const affirmations = data.affirmations;
-        
-        const button = document.getElementById('showAffirmationButton');
-        const affirmationContainer = document.getElementById('affirmationContainer');
-            
-        function displayRandomAffirmation() {
-            const randomIndex = Math.floor(Math.random() * affirmations.length);
-            const affirmation = affirmations[randomIndex];
+  .then(response => response.json())
+  .then(data => {
+    console.log('Fetched data:', data);
 
-            const affirmationElement = document.createElement('p');
-            affirmationElement.textContent = affirmation.description;   
-            
-            affirmationContainer.innerHTML = '';
-            affirmationContainer.appendChild(affirmationElement);
+    if (!Array.isArray(data)) {
+      throw new Error('Fetched data is not in the expected format');
+    }
 
-            button.addEventListener('click', displayRandomAffirmation);
+    const affirmations = data;
+
+    const button = document.getElementById('showAffirmationButton');
+    const affirmationContainer = document.getElementById('affirmationContainer');
+
+    function displayRandomAffirmation() {
+      const randomIndex = Math.floor(Math.random() * affirmations.length);
+      const affirmation = affirmations[randomIndex];
+
+      const affirmationElement = document.createElement('p');
+      affirmationElement.textContent = affirmation.description;
+
+      affirmationContainer.innerHTML = '';
+      affirmationContainer.appendChild(affirmationElement);
+    }
+
+    button.addEventListener('click', displayRandomAffirmation);
   })
-    
-        };
-    
+  .catch(error => {
+    console.error('Error fetching affirmations:', error);
+  });
