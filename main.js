a85795a13e1612624316d144388c52b49538d4b6
Franklin Hau <fhauchi@gmail.com>
2023-05-11 12:57:38 -0600

    added a div class=container

--- main.js
+++ main.js
@@ -1,3 +1,11 @@
 console.log('main js works')
 
-fetch('http://localhost:3000/affirmations').then(response => response.json()).then(json => console.log(json))
+fetch('http://localhost:3000/affirmations')
+    .then(response => response.json())
+   // .then(json => console.log(json))
+    .then(affirmations) => {
+        const container = document.querySelector('ul#confirmationList');
+        affirmations.forEach(affirmation => {
+            
+        });
+    }
