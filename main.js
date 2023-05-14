426632974f3ad6cfb4ad32674b4632a40a7283a4
Franklin Hau <fhauchi@gmail.com>
2023-05-14 07:27:08 -0600

    I was missing an iteration method

--- main.js
+++ main.js
@@ -1,44 +1,58 @@
+
+//Fetching data from the server at the specified URL, which returns an array of affirmations.
 fetch('http://localhost:3000/affirmations')
-  .then(response => response.json())
-  .then(data => {
-    //console.log('Fetched data:', data);
+  .then(response => response.json())//Convert the response received from the server into a JSON object.
+  .then(data => {  //Processing the JSON object received in the previous step.
+    const affirmations = data; // Store the data received from the server in a variable named "affirmations".
+    const button = document.getElementById('showAffirmationButton');//Get the HTML element with the specified ID
+    const affirmationContainer = document.getElementById('affirmationContainer');//Get element with the specified ID
 
-    // if (!Array.isArray(data)) {
-    //   throw new Error('not in the expected format');
-    // }
+    let currentIndex = 0;//will keep track of current index of 'Affirmations' array 
 
-    const affirmations = data;
-
-    const button = document.getElementById('showAffirmationButton');
-    const affirmationContainer = document.getElementById('affirmationContainer');
-
+    //creates a new smiley element 
     function createSmileyElement() {
       const smiley = document.createElement('span');
       smiley.textContent = '😊';
       smiley.classList.add('smiley');
       return smiley;
     }
+    //This function displays the next affirmation from the affirmations array.
+    function displayNextAffirmation() {
+      if (currentIndex < affirmations.length) { //checks if currentIndex is within the length 
+        const affirmation = affirmations[currentIndex];
+        affirmationContainer.innerHTML = '';
 
-    function displayRandomAffirmation() {
-      const randomIndex = Math.floor(Math.random() * affirmations.length);
-      const affirmation = affirmations[randomIndex];
+        //forEach iterates through each element in the affirmations array, passing the current 
+        //element (affirmation) and its index (index) as arguments to the callback function.
+        affirmations.forEach((affirmation, index) => { //
+          if (index === currentIndex) {
+            const affirmationElement = document.createElement('p');//creates a new 'p'
+            affirmationElement.textContent = affirmation.description;//replaces with new affirmation
+            affirmationElement.classList.add('affirmation');//adds the class 'affirmation' to the affirmationElement
 
-      const affirmationElement = document.createElement('p');
-      affirmationElement.textContent = affirmation.description;
-      affirmationElement.classList.add('affirmation');
+            //mouseover event listener to the paragraph element. When the mouse is hovered 
+            //over the element, the text content is removed, and a smiley element is added 
+            affirmationElement.addEventListener('mouseover', () => {
+              affirmationElement.textContent = '';
+              const smiley = createSmileyElement();
+              affirmationContainer.appendChild(smiley);
+            });
 
-      affirmationElement.addEventListener('mouseover', () => {
-        affirmationElement.textContent = '';
-        const smiley = createSmileyElement();
-        affirmationContainer.appendChild(smiley);
-      })
-
-      affirmationContainer.innerHTML = '';
-      affirmationContainer.appendChild(affirmationElement);
+            //appending the affirmationElement to the affirmationContainer element in the DOM.
+            affirmationContainer.appendChild(affirmationElement);
+          }
+        });
+        //next affirmation will be displayed when the button is clicked again.
+        currentIndex++;
+      } else { //end of the array has been reached, display an alert message
+        alert("You've reached the end of the affirmations.");
+      }
     }
 
-    button.addEventListener('click', displayRandomAffirmation);
+    //click event listener to the button.
+    button.addEventListener('click', displayNextAffirmation);
   })
+  //Catch any errors that occur during the fetch process
   .catch(error => {
     console.error('Error fetching affirmations:', error);
   });
